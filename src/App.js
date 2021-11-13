cd42e16e495043e70be50aa17772f9907ac47d3e
Kola <87288429+KXLAA@users.noreply.github.com>
2021-11-13 16:04:17 +0100

    remove boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import CompletedCv from "./components/cv/CompletedCv";
+import Form from "./components/form/Form";
 
 function App() {
+  //Personal History
+  const [infoInput, setInfoInput] = useState({
+    fullName: " ",
+    email: " ",
+    number: " ",
+    location: " ",
+    summary: " ",
+  });
+
+  const handleInfoChange = ({ target }) => {
+    setInfoInput((prevInfoData) => ({
+      ...prevInfoData,
+      [target.name]: target.value,
+    }));
+    console.log(infoInput);
+  };
+
+  //Education
+  const [eduInputList, setEduInputList] = useState([
+    {
+      schoolName: " ",
+      degree: " ",
+      course: " ",
+      location: " ",
+      startDate: " ",
+      endDate: " ",
+    },
+  ]);
+
+  const handleEduChange = (event, index) => {
+    const { name, value } = event.target;
+    const list = [...eduInputList];
+    list[index][name] = value;
+    setEduInputList(list);
+  };
+
+  // handle click event of the Remove button
+  const handleRemoveClick = (index) => {
+    const list = [...eduInputList];
+    list.splice(index, 1);
+    setEduInputList(list);
+  };
+
+  // handle click event of the Add button
+  const handleAddClick = (event) => {
+    event.preventDefault();
+    setEduInputList([
+      ...eduInputList,
+      {
+        schoolName: " ",
+        degree: " ",
+        course: " ",
+        location: " ",
+        startDate: " ",
+        endDate: " ",
+      },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Form
+              handleInfoChange={handleInfoChange}
+              infoInput={infoInput}
+              //
+              handleEduChange={handleEduChange}
+              eduInputList={eduInputList}
+              handleAddClick={handleAddClick}
+              handleRemoveClick={handleRemoveClick}
+            />
+          }
+        />
+        <Route path="/completed" element={<CompletedCv />} />
+      </Routes>
+    </>
   );
 }
 
